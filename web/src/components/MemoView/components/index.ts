cfc4bf766dd933ff25b4ebc29e7d144fd92efe0c
GoreeCloud <295260680+GoreeCloud@users.noreply.github.com>
2026-08-12 17:47:58 -0500

    feat(notes): export direct card actions

--- web/src/components/MemoView/components/index.ts
+++ web/src/components/MemoView/components/index.ts
@@ -1,3 +1,4 @@
+export { default as GoreeCloudCardActions } from "./GoreeCloudCardActions";
 export { default as MemoBody } from "./MemoBody";
 export { default as MemoCommentListView } from "./MemoCommentListView";
 export { default as MemoHeader } from "./MemoHeader";
